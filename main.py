ab49306d6a25bc25f71cbdeea63b3f2eecb9e874
Harshitha <harshithav@951gmail.com>
2022-11-13 18:16:49 +0530

    Team 6

---
+++ main.py
@@ -0,0 +1,12 @@
+Even numbers
+startpoint = int(input("Enter start point: "))
+endpoint = int(input("Enter end point: "))
+for num in range(startpoint+1, endpoint):
+   if(num%2==0):
+       print(num)
+odd numbers
+startpoint = int(input("Enter start point: "))
+endpoint = int(input("Enter end point: "))
+for num in range(startpoint+1, endpoint):
+   if(num%2!==0):
+       print(num)
