f45ce909f3189b23ccbb86bc3e2790bc7ada63d1
Harshitha V <117572809+Harshithav83@users.noreply.github.com>
2022-11-19 14:53:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-Even numbers
-startpoint = int(input("Enter start point: "))
-endpoint = int(input("Enter end point: "))
-for num in range(startpoint+1, endpoint):
-   if(num%2==0):
-       print(num)
-odd numbers
-startpoint = int(input("Enter start point: "))
-endpoint = int(input("Enter end point: "))
-for num in range(startpoint+1, endpoint):
-   if(num%2!==0):
-       print(num)
-Factorial of numbers
-n = int(input("enter the number:"))
-result = 1
-for i in range (n,0,-1):
-    result = result*i
-    print("factorial of ", n "is", result)
-sum of numbers
-list1 = [10,20,60]
-sum_list1 = 0
-for i in list1:
-    sum_list1 = sum_list1 +i
-    print("sum of list is:",sum_list1)
-    Average of numbers
-    num = int(input("How many numbers"))
-    totalsum = 0
-    for n in range (num):
-        numbers = float(input("Enter any number"))
-        totalsum += numbers
-        avg = totalsum/num
-
+endpoint = int(input("Enter the value of endpoint"))
+sumofnumbers = 0
+i = 0
+while i <= endpoint:
+    sumofnumbers = sumofnumbers + i
+    i = i + 1
+    print("The sum of first {} whole numbers is {}".format(endpoint, sumofnumbers))
