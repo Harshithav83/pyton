bd2de686b84543685283f3db2d8e002d0fcc579b
Harshitha V <117572809+Harshithav83@users.noreply.github.com>
2022-11-19 17:04:25 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,8 @@
+startpoint = int(input("Enter the start point"))
+endpoint = int(input("Enter the end point"))
+for i in range(startpoint,endpoint):
+    pass
+if startpoint<endpoint:
+    pass
+else:
+    print("Hello")
