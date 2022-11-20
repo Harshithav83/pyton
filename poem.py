fcc1fddbd461030a6cff703cf5821188b1a8b081
Harshitha V <117572809+Harshithav83@users.noreply.github.com>
2022-11-20 19:26:20 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,9 @@
+single_quote_string = 'Python is fun'
+print(single_quote_string)
+double_quote_string = "Python is joy"
+print(double_quote_string)
+triple_quote_string = """Python is easy"""
+print(triple_quote_string)
+print("""Python is fun
+            Python is joy
+               Python is easy""")
